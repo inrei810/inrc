aa80856db7b26debcc13013541a2c375e78ef61b
inrei810 <91014423+inrei810@users.noreply.github.com>
2022-09-01 23:25:49 +0900

    Add files via upload

---
+++ remotecontrolh/remotecontrolh/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace remotecontrolh
+{
+    static class Program
+    {
+        static public bool Hidestart = false;
+        static public bool Restart = false;
+
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string mutexName = "REMOKONSNDDAD";
+            //Mutexオブジェクトを作成する
+            System.Threading.Mutex mutex = new System.Threading.Mutex(false, mutexName);
+            bool hasHandle = false;
+            try
+            {
+                try
+                {
+                    //ミューテックスの所有権を要求する
+                    hasHandle = mutex.WaitOne(0, false);
+                }
+                //.NET Framework 2.0以降の場合
+                catch (System.Threading.AbandonedMutexException)
+                {
+                    //別のアプリケーションがミューテックスを解放しないで終了した時
+                    hasHandle = true;
+                }
+                //ミューテックスを得られたか調べる
+                if (hasHandle == false)
+                {
+                    //得られなかった場合は、すでに起動していると判断して終了
+                    MessageBox.Show("多重起動はできません。\rタスクトレイを見てみてください","エラー",MessageBoxButtons.OK,MessageBoxIcon.Stop);
+
+                    return;
+                }
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                foreach (string s in args)
+                {
+                    if (string.Join("", args) == "HIDESTART")
+                    {
+                        Hidestart = true;
+                    }
+                   else if (string.Join("", args) == "RESTART")
+                    {
+                        Restart = true;
+                    }
+                }
+           
+
+
+                Application.Run(new Form1());
+            }
+            finally
+            {
+                if (hasHandle)
+                {
+                    //ミューテックスを解放する
+                    mutex.ReleaseMutex();
+                }
+                mutex.Close();
+            }
+        }
+        static bool Hideinfo()
+        {
+            return Hidestart;
+        }
+    }
+}
